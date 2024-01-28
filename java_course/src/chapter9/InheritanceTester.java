ab6d55380e9e679c7a034e2a2cf3c088d41e1c16
Dawn-Marie <dunn.dawnmarie@gmail.com>
2024-01-28 15:07:34 -0500

    Java Programming Chapter 9a example

---
+++ java_course/src/chapter9/InheritanceTester.java
@@ -0,0 +1,17 @@
+package chapter9;
+
+/*
+Chapter 9a Inheritance Example
+This class demonstrates that Employee objects have all the same
+methods available as the Person object, plus methods specific
+to Employee.
+ */
+public class InheritanceTester {
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        Employee employee = new Employee();
+
+    }
+}
