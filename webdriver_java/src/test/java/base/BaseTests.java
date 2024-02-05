b66e5f057fed92dbe98871d90f552aa77097e8e4
Dawn-Marie <dunn.dawnmarie@gmail.com>
2024-02-04 21:36:55 -0500

    Selenium WebDriver with Java Chapter 4.2 example

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -2,25 +2,26 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
 public class BaseTests {
 
-    private WebDriver driver;
     protected HomePage homePage;
+    private WebDriver driver;
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
-
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.edge.driver", "resources/msedgedriver.exe");
         driver = new EdgeDriver();
         driver.get("https://the-internet.herokuapp.com");
 
         homePage = new HomePage(driver);
+    }
 
+    @AfterClass
+    public void tearDown() {
         driver.quit();
     }
 }
