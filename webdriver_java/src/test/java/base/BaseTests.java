bb3bc7366cd3a57c3602453104ead82f6804e985
Dawn-Marie <dunn.dawnmarie@gmail.com>
2024-02-04 19:29:58 -0500

    Selenium WebDriver with Java Chapter 3 example

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -1,26 +1,33 @@
 package base;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
 
     public void setUp() {
         System.setProperty("webdriver.edge.driver", "resources/msedgedriver.exe");
         driver = new EdgeDriver();
         driver.get("https://the-internet.herokuapp.com");
 
-        driver.manage().window().setSize(new Dimension(375, 812));
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
 
         System.out.println(driver.getTitle());
         driver.quit();
-    }
-
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
     }
 }
