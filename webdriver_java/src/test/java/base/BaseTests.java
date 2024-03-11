ee3bc2bafe4c1257397274aae97b374d69eb22b0
Dawn-Marie Dunn <ddunn@andovercos.com>
2024-03-11 08:45:02 -0400

    Selenium WebDriver with Java Chapter 12 example

--- webdriver_java/src/test/java/base/BaseTests.java
+++ webdriver_java/src/test/java/base/BaseTests.java
@@ -1,9 +1,16 @@
 package base;
 
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
@@ -27,6 +34,20 @@ public class BaseTests {
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com");
         homePage = new HomePage(driver);
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+                System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
